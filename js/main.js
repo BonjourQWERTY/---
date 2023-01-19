917a9d008d0707da8423cde3bcd98291ab4a533d
Bonjour <artyom.starchikov4@gmail.com>
2023-01-19 23:10:24 +0300

    адаптив

--- js/main.js
+++ js/main.js
@@ -17,7 +17,21 @@ inputRequestOverlay.addEventListener('click', handleCloseRequestDialog);
 
 
 
+function onToggle(event) {
+  if (event.target.open) {
+    document.querySelectorAll(".how-it-works__agreements-group > details[open]").forEach((el) => {
+      // Исключаем из перебора елемент который мы только что открыли
+      if (el === event.target) {
+        return;
+      }
 
+      // Закрываем все остальные елементы <details>
+      el.open = false;
+    });
+  }
+}
 
-
-
+// Вешаем наблюдатель на все елементы <details> внутри акордиона
+document
+  .querySelectorAll(".how-it-works__agreements-group > details")
+  .forEach((el) => el.addEventListener("toggle", onToggle));
